155fea30e9f408801009d741446653cbd47972cf
Bright Jonathan <brightjonathan000@gmail.com>
2023-01-10 15:07:22 +0100

    starter Repo

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+// MYAPP Widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, 
+        home: NextPage());
+  }
+}
+
+class NextPage  extends StatelessWidget {
+  const NextPage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' App Again '),
+      ),
+    );
+  }
+}
